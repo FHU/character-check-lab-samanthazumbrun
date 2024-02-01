4a9ad87f3b2a5e475599a5f60d4ccf3934b6f05c
samanthazumbrun <samantha.zumbrun@students.fhu.edu>
2024-02-01 12:36:30 -0600

    main.py
    added code to defcheck_character

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-#Remove pass and complete the code
 def check_character(word, index):
-   pass
+    char = word[index]
+    type = ''
+    if char.isalpha():
+        type = 'letter'
+    elif char.isspace():
+        type = 'white space'
+    elif char.isdigit():
+        type = 'digit'
+    else:
+        type = 'unknown'
+    
+    return type
+            
+
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
